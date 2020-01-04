589191578749f131663f1bbcbff52d69cfec5ada
Vyacheslav <vyacheslavsaloid.work@gmail.com>
2020-01-04 14:28:51 +0200

    fix bugs and and some new design

--- src/app/routes/main/component/main.component.ts
+++ src/app/routes/main/component/main.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ContactsService } from '../services/contacts/contacts.service';
 @Component({
   selector: 'app-main-root',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   public showSidebar = true;
 
   constructor(private contactsService: ContactsService) {}
+  isShowSidebar = () =>
+    (this.showSidebar = window.innerWidth > 990 ? true : false);
+
+  @HostListener('window:resize', ['$event'])
+  onResize = () => this.isShowSidebar();
 
   toogleSidebar = () => (this.showSidebar = !this.showSidebar);
 
   getContacts = () => this.contactsService.getContacts();
+
+  ngOnInit() {
+    this.isShowSidebar();
+  }
 }
