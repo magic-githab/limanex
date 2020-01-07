abb345561a893d62060db6ea8a382f95573c0481
Vyacheslav <vyacheslavsaloid.work@gmail.com>
2020-01-07 15:16:08 +0200

    fix conflicts

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { AuthComponent } from './component/auth.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AuthRoutingModule]
+  declarations: [AuthComponent],
+  imports: [SharedModule, AuthRoutingModule]
 })
 export class AuthModule {}
